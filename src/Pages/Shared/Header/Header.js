84d1f5767ae77de2432e5e0d137da5ac22898cdf
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-12 14:20:42 +0600

    created logo, added navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
+import logo from '../../../images/logo.png'
 const Header = () => {
     return (
         <header>
-            <h2>This is header</h2>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"><img src={logo} alt="" height="35px" /></Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     );
 };
