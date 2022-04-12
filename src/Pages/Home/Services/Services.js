9eb5306e054e0f715e30c854afea0a62a0a48cfa
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-12 14:56:53 +0600

    project finished

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,8 +12,8 @@ const Services = () => {
     },[])
 
     return (
-        <div className='container'>
-            <h1 className='text-primary text-center mt-5'>Our Services</h1>
+        <div id="services" className='container'>
+            <h1 className='text-primary text-center mt-5 mb-5'>Our Services</h1>
             <div className="services-container">
             {
                 services.map(service => <Work key={service.id} service={service}></Work>)
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,8 +14,8 @@ const Header = () => {
                     <div>
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href="#services">Services</Nav.Link>
+                            <Nav.Link href="#experts">Experts</Nav.Link>
                         </Nav>
                     </div>
                 </Container>
