cdfa363bc7a3eefe4c85ad23c7381b7ad4ae7321
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-14 12:52:20 +0600

    created 404 page, added login route

---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='container'>
+            <h2 className='text-primary text-center'>Error Nothing Found</h2>
+            <img className='w-100 rounded-3 text-center' src="https://images.unsplash.com/photo-1534696225531-6f1501f40411?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
