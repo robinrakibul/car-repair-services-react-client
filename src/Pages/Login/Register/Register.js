d3d16e041c83bcfc3dadd6c410f2e476bed35df9
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-15 12:41:29 +0600

    login, register form edited

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Register.css';
 
 const Register = () => {
+
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
+
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
