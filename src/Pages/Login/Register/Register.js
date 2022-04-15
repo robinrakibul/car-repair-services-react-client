8961ab4b728f778e59c219ea7553c74c0796e424
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-15 12:44:27 +0600

    login, register toggle done

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
-
+    const navigate = useNavigate();
+    const navigateLogin = () =>{
+        navigate('/login')
+    }
     return (
         <div className='register-form'>
             <h2 style={{ textAlign: 'center' }}>Please Register</h2>
@@ -15,6 +19,7 @@ const Register = () => {
 
                 <input type="submit" value="Register" />
             </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
