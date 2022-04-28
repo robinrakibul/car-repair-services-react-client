ca3bc96f75dfcdf3aed296bbdf1c695e7503dd90
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-28 10:44:07 +0600

    created form in checkout page

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,13 +1,53 @@
+import axios from 'axios';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../../firebase.init';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const Checkout = () => {
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
+    const [user] = useAuthState(auth);
+
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+        axios.post('http://localhost:5000/order', order)
+            .then(response => {
+                const { data } = response;
+                if (data.insertedId) {
+                    toast('Your order is booked!!!');
+                    event.target.reset();
+                }
+            })
+    }
+
     return (
-        <div>
-            <h2 className='container mt-2'>Please Order: {service.name}</h2>
+        <div className='w-50 mx-auto rounded'>
+            <h2 className='container mt-2 mb-2'>Please Order: {service.name}</h2>
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" value={user?.displayName} name="name" placeholder='Your Name' required readOnly disabled />
+                <br />
+                <input className='w-100 mb-2' type="email" value={user?.email} name="email" placeholder='Your Email' required readOnly disabled />
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='Service' required readOnly />
+                <br />
+                <input className='w-100 mb-2' type="text" name="address" placeholder='Home Address' autoComplete='off' required />
+                <br />
+                <input className='w-100 mb-2' type="text" name="phone" placeholder='Phone Number' required />
+                <br />
+
+                <input className='btn btn-dark mt-3' type="submit" value="Place Order" />
+
+            </form>
         </div>
     );
 };
