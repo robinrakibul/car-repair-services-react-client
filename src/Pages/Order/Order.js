502c8598ac8d33ed594fc8a86267a430a35ac6be
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-28 16:22:43 +0600

    fix issues to show how many orders perticular user

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Order = () => {
-    const [user] = useAuthState
+    const [user] = useAuthState(auth);
     const [orders,setOrders] = useState([]);
     useEffect(()=>{
         const getOrders = async() =>{
-            const url = `http://127.0.0.1:5000/orders`;
+            const email = user.email;
+            const url = `http://127.0.0.1:5000/orders?email=${email}`;
             const {data} = await axios.get(url);
             setOrders(data);
         }
         getOrders();
-    })
+    },[user])
     return (
         <div className='container mt-3'>
             <h3>Your Orders {orders.length}</h3>
