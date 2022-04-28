5b55f16f2117c966066a085a17c0261c711696b4
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-28 21:40:09 +0600

    fix error

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -10,7 +10,7 @@ const Order = () => {
     useEffect(()=>{
         const getOrders = async() =>{
             const email = user.email;
-            const url = `http://127.0.0.1:5000/orders?email=${email}`;
+            const url = `https://pure-garden-17603.herokuapp.com/orders?email=${email}`;
             const {data} = await axios.get(url);
             setOrders(data);
         }
